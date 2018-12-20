651a3554784929972aaa822ed40222c048156bdb
Alberto Perez <aperez@alea-soluciones.com>
2018-12-20 12:04:56 +0100

    [INTERNAL] Rename package name to infcommon instead infcommon3

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='infcommon3',
+setup(name='infcommon',
       version='0.0.1',
       author='Bifer Team',
       description='Common Infrastructure Utilities',
       platforms='Linux',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'specs', 'integration_specs'])
+      packages=find_packages(exclude=['tests', 'integration_tests', 'specs', 'integration_specs'])
       )
 
