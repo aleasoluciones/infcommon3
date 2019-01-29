4e83f2efad78390464f62782cab9fe45c00c9642
Jorge Senín <jsenin@alea-soluciones.com>
2019-01-29 12:37:28 +0100

    [INTERNAL] Moved modules dependencies from requirements.txt o setup.py
    For automatic installation modules, whe use install_require at setup.py to install

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setup(name='infcommon',
       author='Bifer Team',
       description='Common Infrastructure Utilities',
       platforms='Linux',
-      packages=find_packages(exclude=['tests', 'integration_tests', 'specs', 'integration_specs'])
+      packages=find_packages(exclude=['tests', 'integration_tests', 'specs', 'integration_specs']),
+      install_requires=['raven==6.10.0',
+                        'mysqlclient==1.3.14',
+                        'retrying==1.3.3',
+                        'psycopg2==2.7.6.1',
+                        'psycopg2-binary==2.7.7',
+                        'PyYAML==4.2b4',
+                        ]
       )
 
