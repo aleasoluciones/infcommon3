ce9c561e3b5f4112b6bbc6fd6798fbe1cfa8f187
Jorge Senín <jsenin@alea-soluciones.com>
2019-05-07 11:34:28 +0200

    [REFACTOR] Postgresql Factory use ids to allow many postgresql instances
    Singleton pattern does not allow to instance two postgresql client with diferent host, port, db_name params. Because postgresql_client ID is a static string, so second, third instances are returning the first one

--- infcommon/postgres/factory.py
+++ infcommon/postgres/factory.py
@@ -8,21 +8,22 @@ from infcommon.postgres.postgres import PostgresClient
 
 def postgres_client_from_connection_parameters(user, password, host, port, db_name, use_dict_cursor=None):
     connection_uri = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(user=user, password=password, host=host, port=port, db_name=db_name)
-    cursor_factory = _cursor_factory(use_dict_cursor)
-    return Factory.instance('posgres_client_from_connection_parameters',
-                            lambda: PostgresClient(connection_uri, cursor_factory=cursor_factory)
-                           )
+    return _postgres_client(connection_uri, use_dict_cursor)
 
 # TODO: remove this factory method. db_uri should be passed by application, not
 # at factory
 def postgres_client_from_connection_os_variable(db_uri_os_valiable_name='LOCAL_PG_DB_URI', use_dict_cursor=None):
     print("This method `postgres_client_from_connection_os_variable` should not be used. Please consider to use another factory")
     connection_uri = os.getenv(db_uri_os_valiable_name)
+    return _postgres_client(connection_uri, use_dict_cursor)
+
+
+def _postgres_client(connection_uri, use_dict_cursor):
     cursor_factory = _cursor_factory(use_dict_cursor)
-    return Factory.instance('postgres_client_from_connection_os_variable',
+    instance_id = "postgres_client_{}_{}".format(connection_uri, use_dict_cursor)
+    return Factory.instance(instance_id,
                             lambda: PostgresClient(connection_uri, cursor_factory=cursor_factory)
                            )
-
 
 def _cursor_factory(use_dict_cursor=None):
     if use_dict_cursor:
