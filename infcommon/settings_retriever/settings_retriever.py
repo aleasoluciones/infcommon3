cdf4c4123d1c56f1ded912fb437c7ffcea85d8b8
luisddm <luisdedios@alea-soluciones.com>
2019-05-07 12:59:55 +0200

    [FEATURE] Add methods to SettingsRetriever to get boolean and integer values

--- infcommon/settings_retriever/settings_retriever.py
+++ infcommon/settings_retriever/settings_retriever.py
@@ -1,4 +1,6 @@
 class SettingsRetriever(object):
+    TRUTHY_VALUES = [True, 1, 'True', '1', 'Y']
+
     def __init__(self, envs, settings_file):
         self._envs = envs
         self._settings_file = settings_file
@@ -11,5 +13,15 @@ class SettingsRetriever(object):
         if file_value:
             return file_value
         return default_value
+
+    def get_int(self, key, default_value=None):
+        result_value = self.get_value(key, default_value)
+        return int(result_value)
+
+    def get_bool(self, key, default_value=None):
+        result_value = self.get_value(key, default_value)
+        if result_value in self.TRUTHY_VALUES:
+            return True
+        return False
 
 
--- specs/application_configuration_spec.py
+++ specs/application_configuration_spec.py
@@ -1,4 +1,5 @@
-from expects import expect, equal
+from mamba import describe, context, it, before
+from expects import expect, equal, raise_error
 from doublex import Stub, when
 
 from infcommon.yaml_reader.yaml_reader import YamlReader
@@ -8,36 +9,156 @@ A_KEY = 'a_key'
 AN_ENVIRONMENT_VALUE = 'an_environment_value'
 A_FILE_VALUE = 'a_file_value'
 A_DEFAULT_VALUE = 'a_default_value'
+A_NUMBER_AS_INTEGER = 23
+A_NUMBER_AS_STRING = '23'
+A_NON_PARSEABLE_STRING = '5ty'
+A_TRUTHY_VALUE_TRUE = 'True'
+A_TRUTHY_VALUE_TRUE_BOOL = True
+A_TRUTHY_VALUE_ONE = '1'
+A_TRUTHY_VALUE_ONE_NUMERIC = 1
+A_TRUTHY_VALUE_Y = 'Y'
+A_FALSY_VALUE_FALSE = 'False'
+A_FALSY_VALUE_ZERO = '0'
+A_FALSY_VALUE_N = 'N'
+ANYTHING_ELSE = 'f5kN'
 
-with describe('Settings'):
+with describe('Settings') as self:
     with before.each:
         self.settings_file = Stub(YamlReader)
 
-    with context('when enviroment variable is present'):
-        with it('returns enviroment value'):
-            envs = {A_KEY: AN_ENVIRONMENT_VALUE}
-            settings = SettingsRetriever(envs, self.settings_file)
+    with context('FEATURE: get string value'):
+        with context('when enviroment variable is present'):
+            with it('returns enviroment value'):
+                envs = {A_KEY: AN_ENVIRONMENT_VALUE}
+                settings = SettingsRetriever(envs, self.settings_file)
 
-            value = settings.get_value(key=A_KEY)
+                value = settings.get_value(key=A_KEY)
 
-            expect(value).to(equal(AN_ENVIRONMENT_VALUE))
+                expect(value).to(equal(AN_ENVIRONMENT_VALUE))
 
-    with context('when enviroment variable is NOT present'):
-      with context('variable file is present'):
-        with it('returns file value'):
-            envs = {}
-            when(self.settings_file).get(A_KEY).returns(A_FILE_VALUE)
-            settings = SettingsRetriever(envs, self.settings_file)
+        with context('when enviroment variable is NOT present'):
+            with context('variable file is present'):
+                with it('returns file value'):
+                    envs = {}
+                    when(self.settings_file).get(A_KEY).returns(A_FILE_VALUE)
+                    settings = SettingsRetriever(envs, self.settings_file)
 
-            value = settings.get_value(key=A_KEY)
+                    value = settings.get_value(key=A_KEY)
 
-            expect(value).to(equal(A_FILE_VALUE))
+                    expect(value).to(equal(A_FILE_VALUE))
 
-      with context('variable file is NOT Present'):
-        with it('returns the default value'):
-            envs = {}
-            settings = SettingsRetriever(envs, self.settings_file)
+            with context('variable file is NOT present'):
+                with it('returns the default value'):
+                    envs = {}
+                    settings = SettingsRetriever(envs, self.settings_file)
 
-            value = settings.get_value(key=A_KEY, default_value=A_DEFAULT_VALUE)
+                    value = settings.get_value(key=A_KEY, default_value=A_DEFAULT_VALUE)
 
-            expect(value).to(equal(A_DEFAULT_VALUE))
+                    expect(value).to(equal(A_DEFAULT_VALUE))
+
+    with context('FEATURE: get parsed integer value'):
+        with context('when an environment variable can be parsed to int'):
+            with it('returns an integer'):
+                envs = {A_KEY: A_NUMBER_AS_STRING}
+                settings = SettingsRetriever(envs, self.settings_file)
+
+                value = settings.get_int(key=A_KEY)
+
+                expect(value).to(equal(A_NUMBER_AS_INTEGER))
+
+        with context('when an environment variable cannot be parsed to int'):
+            with it('raises an exception'):
+                envs = {A_KEY: A_NON_PARSEABLE_STRING}
+                settings = SettingsRetriever(envs, self.settings_file)
+
+                def _get_int():
+                    return settings.get_int(key=A_KEY)
+
+                expect(_get_int).to(raise_error(ValueError))
+
+
+    with context('FEATURE: get parsed boolean value'):
+        with context('when an environment variable can be parsed to boolean'):
+            with context('when truthy values'):
+                with context('when a variable is True as a String'):
+                    with it('returns True'):
+                        envs = {A_KEY: A_TRUTHY_VALUE_TRUE}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(True))
+
+                with context('when a variable is 1 as a String'):
+                    with it('returns True'):
+                        envs = {A_KEY: A_TRUTHY_VALUE_ONE}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(True))
+
+                with context('when a variable is Y'):
+                    with it('returns True'):
+                        envs = {A_KEY: A_TRUTHY_VALUE_Y}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(True))
+
+                with context('when a variable is True as a Boolean'):
+                    with it('returns True'):
+                        envs = {A_KEY: A_TRUTHY_VALUE_ONE_NUMERIC}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(True))
+
+                with context('when a variable is 1 as a Number'):
+                    with it('returns True'):
+                        envs = {A_KEY: A_TRUTHY_VALUE_TRUE_BOOL}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(True))
+
+
+            with context('when falsy values'):
+                with context('when a variable is False'):
+                    with it('returns False'):
+                        envs = {A_KEY: A_FALSY_VALUE_FALSE}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(False))
+
+                with context('when a variable is 0'):
+                    with it('returns False'):
+                        envs = {A_KEY: A_FALSY_VALUE_ZERO}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(False))
+
+                with context('when a variable is N'):
+                    with it('returns False'):
+                        envs = {A_KEY: A_FALSY_VALUE_N}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(False))
+
+                with context('when a variable is anything else'):
+                    with it('returns False'):
+                        envs = {A_KEY: ANYTHING_ELSE}
+                        settings = SettingsRetriever(envs, self.settings_file)
+
+                        value = settings.get_bool(key=A_KEY)
+
+                        expect(value).to(equal(False))
