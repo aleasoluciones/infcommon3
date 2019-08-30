582914a80e1c4d39cc33c8b4e09004ccb536364b
Alberto de la Cruz <acruz@alea-soluciones.com>
2019-08-30 11:52:55 +0200

    [FIX] Factory tests

---
+++ dev/run_factory_tests.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from infcommon import generic_factory_test
+
+generic_factory_test.run()
--- infcommon/postgres/factory.py
+++ infcommon/postgres/factory.py
@@ -7,12 +7,12 @@ from infcommon.factory import Factory
 from infcommon.postgres.postgres import PostgresClient
 
 
-def postgres_client_from_connection_parameters(user, password, host, port, db_name, use_dict_cursor=None):
+def postgres_client_from_connection_parameters(user=None, password=None, host=None, port=None, db_name=None, use_dict_cursor=None):
     connection_uri = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(user=user, password=password, host=host, port=port, db_name=db_name)
     return _postgres_client(connection_uri, use_dict_cursor)
 
 
-def _postgres_client(connection_uri, use_dict_cursor):
+def _postgres_client(connection_uri=None, use_dict_cursor=None):
     cursor_factory = _cursor_factory(use_dict_cursor)
     instance_id = "postgres_client_{}_{}".format(connection_uri, use_dict_cursor)
     return Factory.instance(instance_id,
