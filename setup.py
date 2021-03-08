ed1efd907d8cbc8e834d954166b9974507193723
Jorge Senín <jorge@senin.org>
2021-03-08 11:07:45 +0100

    [INFRASTRUCTURE] Upgrade psycopg2 to 2.8.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='infcommon',
       install_requires=['raven==6.10.0',
                         'mysqlclient==1.4.1',
                         'retrying==1.3.3',
-                        'psycopg2==2.7.7',
+                        'psycopg2==2.8.6',
                         'PyYAML==4.2b4',
                         'jsonpickle==1.2'
                         ]
