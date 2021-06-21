23212754759e9d63f6a3eedd315da439f8f839e6
Jorge Senín <jsenin@alea-soluciones.com>
2021-06-21 13:57:39 +0200

    Bump sentry-sdk to 1.10

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='infcommon',
                                       'tests',
                                       'specs',
                                       'integration_specs']),
-      install_requires=['sentry-sdk==1.0.0',
+      install_requires=['sentry-sdk==1.1.0',
                         'aiocontextvars==0.2.2',
                         'PyYAML==4.2b4',
                         'jsonpickle==1.2'
