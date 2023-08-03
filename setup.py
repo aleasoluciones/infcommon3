6b282d4c339a2493b881db19cb11920fd8002c14
Alberto <acruz@alea-soluciones.com>
2023-08-03 07:54:03 +0200

    Revert " SKADE-142 [FIX] Missing lib in setup.py"
    This reverts commit b1c39ae49e7dfd5f712856b9cd41c8c11d7578c3.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(name='infcommon',
       install_requires=['sentry-sdk==1.15.0',
                         'aiocontextvars==0.2.2',
                         'PyYAML==6.0',
-                        'jsonpickle==3.0.1',
-                        'pyyaml-include==1.3.1'
+                        'jsonpickle==3.0.1'
                         ]
       )
