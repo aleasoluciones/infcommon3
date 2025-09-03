9936aef3a58f3744fa48bd18ae1c1ab7e2011edc
Acruz <acruz@alea-soluciones.com>
2025-09-03 07:26:01 +0200

    SKADE-1168 [FEATURE] Add trace_id to logger

--- infcommon/logging_utils.py
+++ infcommon/logging_utils.py
@@ -1,25 +1,42 @@
 import logging
 import logging.config
 import signal
+import contextvars
 
-BASE_CONF= {
+trace_id_var = contextvars.ContextVar("trace_id", default=None)
+
+class TraceIdFilter(logging.Filter):
+    def filter(self, record):
+        record.trace_id = trace_id_var.get()
+        return True
+
+
+BASE_CONF = {
     'version': 1,
     'disable_existing_loggers': False,
 
     'formatters': {
         'console': {
-            'format': '[%(asctime)s][%(levelname)s] %(name)s %(filename)s:%(funcName)s:%(lineno)d | %(message)s',
+            'format': '[%(asctime)s][%(levelname)s][trace_id=%(trace_id)s] '
+                      '%(name)s %(filename)s:%(funcName)s:%(lineno)d | %(message)s',
             'datefmt': '%H:%M:%S',
-            },
         },
+    },
+
+    'filters': {
+        'trace_id': {
+            '()': TraceIdFilter, 
+        },
+    },
 
     'handlers': {
         'console': {
             'level': 'INFO',
             'class': 'logging.StreamHandler',
-            'formatter': 'console'
-            },
+            'formatter': 'console',
+            'filters': ['trace_id'], 
         },
+    },
 
     'loggers': {
         '': {
@@ -36,12 +53,14 @@ def add_handler(handler_name, handler_conf):
     BASE_CONF['loggers']['']['handlers'].append(handler_name)
     logging.config.dictConfig(BASE_CONF)
 
+
 def activate_debug(log_info=True):
     BASE_CONF['handlers']['console']['level'] = 'DEBUG'
     BASE_CONF['disable_existing_loggers'] = True
     logging.config.dictConfig(BASE_CONF)
     if log_info:
         logging.info("Activating debug")
+
 
 def deactivate_debug(log_info=True):
     BASE_CONF['handlers']['console']['level'] = 'INFO'
@@ -50,8 +69,9 @@ def deactivate_debug(log_info=True):
     if log_info:
         logging.info("Deactivating debug")
 
-signal.signal(signal.SIGUSR1, lambda signal, frame: activate_debug())
-signal.signal(signal.SIGUSR2, lambda signal, frame: deactivate_debug())
+
+signal.signal(signal.SIGUSR1, lambda sig, frame: activate_debug())
+signal.signal(signal.SIGUSR2, lambda sig, frame: deactivate_debug())
 
 BASE_CONF['handlers']['console']['level'] = 'INFO'
 logging.config.dictConfig(BASE_CONF)
